52d9494e278a772a235e07bd1d103b729b605c2a
Himanshu Sharma <hs897232@gmail.com>
2026-04-25 15:09:56 +0530

    test

--- backend/app.js
+++ backend/app.js
@@ -6,43 +6,26 @@ import authRoutes from "./routes/authRoutes.js";
 import projectRoutes from "./routes/projectRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 
+const port = process.env.PORT || 5000;
+
 dotenv.config();
 connectDB();
 
 const app = express();
-
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://task-management-iota-five-12.vercel.app"
-];
-
-// ✅ CORS FIX
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS not allowed"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: [
+    "http://localhost:3000",
+    "https://task-management-iota-five-12.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
-
-// ✅ HANDLE PREFLIGHT
-app.options("*", cors());
-
-// Middleware
 app.use(express.json());
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);   
+})
